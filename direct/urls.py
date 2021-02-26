9d824818f1ea93a8583194aee5ca77fff48dc091
himarajab <rajabhima2@gmail.com>
2021-02-26 17:28:27 +0200

    send message

--- direct/urls.py
+++ direct/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from direct.views import people_we_can_message,new_conversation,inbox
+from direct.views import people_we_can_message,new_conversation,inbox,directs,send_direct
 
 urlpatterns = [
     path('start/',people_we_can_message,name='people-we-can-message'),
     path('new/<username>',new_conversation,name='new-conversation'),
+    path('directs/<username>',directs,name='directs'),
+    path('send/',send_direct,name='send-direct'),
     path('',inbox,name='inbox'),
 ]
--- direct/views.py
+++ direct/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponseRedirect, HttpResponseBadRequest, JsonResponse
+from django.urls import reverse
 from django.core.paginator import Paginator
 from django.contrib.auth.models import User
 from direct.models import Message
@@ -52,3 +54,41 @@ def inbox(request):
   }
 
   return render(request,'direct/inbox.html',context)
+
+
+@login_required
+def directs(request,username):
+  user = request.user
+  # load all the conversation
+  messages = Message.get_messages(user=request.user)
+  active_direct = username
+  # messages
+  directs = Message.objects.filter(user=user,recipient__username=username).order_by('-date')
+
+  for message in messages:
+    if message['user'].username == username:
+      # delete all unread messages
+      message['unread'] =0
+  context = {
+      'messages': messages,
+      'directs': directs,
+      'active_direct': active_direct,
+  }
+
+  return render(request,'direct/direct.html',context)
+
+
+@login_required
+def send_direct(request):
+  from_user = request.user
+  to_user_username = request.POST.get('to_user')
+  body = request.POST.get('body')
+
+
+  if request.method == 'POST':
+    to_user = get_object_or_404(User,username=to_user_username)
+    Message.send_message(from_user=from_user,to_user=to_user,body=body)
+    return HttpResponseRedirect(reverse('directs', args=[to_user_username]))
+  else:
+    HttpResponseBadRequest()
+  
